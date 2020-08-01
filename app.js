02fc4501f42161ab307a4cecad0c03a0af449186
Claudio-P <pclaudio435@gmail.com>
2020-08-01 15:37:25 -0300

    Renderizando dados com EJS

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var rotaNoticias = require('./app/routes/noticias')(app);
 var rotaHome = require('./app/routes/home')(app);
 var rotaFormulario = require('./app/routes/formulario_inclusao_noticia')(app);
 
-app.listen(4000, function() {
+app.listen(3000, function() {
     console.log('Running...');
 });
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
     app.get('/noticias', function(req, res) {
         connection.query('SELECT * FROM noticias;', function(error, result) {
             if (error) return console.log(error);
-            res.send(result);
+            res.render('noticias/noticias', {noticias : result});
         });
     });
 };
