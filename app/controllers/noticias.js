cdd8f99ef12bcd640398acb3a4fba4af6ed7c8b5
Claudio-P <pclaudio435@gmail.com>
2020-09-03 16:32:12 -0300

    Projeto Portal Noticias finalizado

--- app/controllers/noticias.js
+++ app/controllers/noticias.js
@@ -5,7 +5,7 @@ module.exports.noticias = function(app, req, res) {
 
         noticiasModel.getNoticias(function(error, result) {
             if (error) return console.log(error);
-            res.render('noticias/noticias', {noticias : result});
+            res.render('noticias/noticias', { noticias : result });
         });
 };
 
@@ -14,7 +14,9 @@ module.exports.noticia = function(app, req, res) {
     var con = app.config.connection();
         var noticiasModel = new app.app.models.NoticiasDAO(con);
 
-        noticiasModel.getNoticia(function(error, result) {
+        var id_noticia = req.query;
+
+        noticiasModel.getNoticia(id_noticia, function(error, result) {
             if (error) return console.log(error);
             res.render('noticias/noticia', { noticia : result });
         });
--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -3,11 +3,12 @@ function NoticiasDAO(con){
 }
 
 NoticiasDAO.prototype.getNoticias = function(callback) {
-    this._con.query('SELECT * FROM noticias;', callback);
+    this._con.query('SELECT * FROM noticias ORDER BY data_criacao DESC;', callback);
 };
 
-NoticiasDAO.prototype.getNoticia = function(callback) {
-    this._con.query('SELECT * FROM noticias WHERE id_noticia = 1;', callback);
+NoticiasDAO.prototype.getNoticia = function(id_noticia, callback) {
+    console.log(id_noticia.id_noticia);
+    this._con.query('SELECT * FROM noticias WHERE id_noticia = ' + id_noticia.id_noticia, callback);
 };
 
 NoticiasDAO.prototype.salvarNoticia = function(noticias, callback) {
